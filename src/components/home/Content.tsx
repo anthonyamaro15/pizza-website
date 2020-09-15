877017b76fa741c540668d274dc44baf5a637a85
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-09-15 16:13:03 -0700

    added footer to homepage

--- src/components/home/Content.tsx
+++ src/components/home/Content.tsx
@@ -11,6 +11,38 @@ const Content = () => {
           <SingleCardComponent />
           <SingleCardComponent />
           <SingleCardComponent />
+        </div>
+      </div>
+      <div className="Content-inner">
+        <h1>thin crust pizza</h1>
+        <div className="items">
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+        </div>
+      </div>
+      <div className="Content-inner">
+        <h1>speciality pizza</h1>
+        <div className="items">
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+        </div>
+      </div>
+      <div className="Content-inner">
+        <h1>frozen pizzas</h1>
+        <div className="items">
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+          <SingleCardComponent />
+        </div>
+      </div>
+      <div className="Content-inner">
+        <h1>popular combos</h1>
+        <div className="items">
           <SingleCardComponent />
           <SingleCardComponent />
           <SingleCardComponent />
