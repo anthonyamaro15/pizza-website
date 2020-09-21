93abbced45058ccbb63d71effc20da823021efca
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-09-21 14:44:54 -0700

    displaying user address now

--- src/components/home/DeliveryModal.tsx
+++ src/components/home/DeliveryModal.tsx
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState, useEffect, SetStateAction } from "react";
 import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
+import axios from "axios";
+
+interface User {
+  address: string;
+  first_name: string;
+  last_name: string;
+  id: number;
+}
 
 const DeliveryModal = () => {
-  const [open, setOpen] = React.useState(false);
+  const [id, setId] = useState<SetStateAction<string> | null>("");
+  const [user, setUser] = useState<User[]>([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const userId = localStorage.getItem("id");
+
+    if (userId) {
+      setId(userId);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (id) {
+      axios
+        .get(`${process.env.REACT_APP_API_URL}/api/cart/user/${id}`)
+        .then((res) => {
+          setUser(res.data);
+        })
+        .catch((err) => {
+          console.log(err.response.data);
+        });
+    }
+  }, [id]);
+
   return (
     <>
       <button className="delivery" onClick={() => setOpen(true)}>
         Delivery
-        <span className="inner-btn">2150 South Canalport Avenue...</span>
+        <span className="inner-btn">
+          {user.length ? user[0].address : "No address"}
+        </span>
       </button>
       <Modal open={open} onClose={() => setOpen(false)}>
         <div className="DeliveryModal">
           <h3>delivered to</h3>
-          <p>2150 South Canalport Avenue Chicago, IL. 60608</p>
+          <p>{user.length ? user[0].address : "No address"}</p>
         </div>
       </Modal>
     </>
