a2cb189ac0917d56bd04fce5bbbf096ce336a9d3
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-09-26 21:10:49 -0700

    added reset password component

--- src/components/home/ForgotPassword.tsx
+++ src/components/home/ForgotPassword.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
+interface InputValues {
+  email: string;
+}
 const ForgotPassword = () => {
+  const { register, handleSubmit, reset } = useForm<InputValues>();
+
+  const onSubmit = (values: InputValues) => {
+    console.log(values);
+    axios
+      .patch(`${process.env.REACT_APP_API_URL}/api/forgotpassword`, values)
+      .then(() => {
+        reset();
+        setTimeout(
+          () => alert("We sent you an email to reset your password!"),
+          600
+        );
+      })
+      .catch((err) => {
+        console.log(err.response.data.errorMessage);
+        alert(err.response.data.errorMessage);
+      });
+  };
   return (
     <div className="SignUp">
       <h1>password reset</h1>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="email">
           email
           <input
@@ -12,6 +35,7 @@ const ForgotPassword = () => {
             name="email"
             id="email"
             placeholder="lou@malnati.com"
+            ref={register({ required: true })}
           />
         </label>
         <button type="submit" className="reset_password">
